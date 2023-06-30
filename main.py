221fa1763ca1dd5c5c2127dd186411d273561fb9
rahul singh shekhawat <rahulsinghshekhawat16@gmail.com>
2023-06-30 12:17:31 +0530

    usage of match case and for loop

---
+++ main.py
@@ -0,0 +1,16 @@
+todos = []
+
+while True:
+    user_action = input("Type add, show or exit: ")
+    user_action.strip()
+
+    match user_action:
+        case "add":
+            todo = input("Enter a todo:")
+            todos.append(todo.capitalize())
+        case "show":
+            for item in todos:
+                print(item)
+        case "exit":
+            break
+
